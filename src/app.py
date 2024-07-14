d3a8b04aa61a6f6fa112b4a4f6b92f1baa7647b7
Davide <davidelaureti54@gmail.com>
2024-07-14 19:43:55 +0900

    anon key

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ from item import Item
 app = Flask(__name__)
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
-SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+SUPABASE_KEY = os.getenv('SUPABASE_ANON_KEY')
 LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 
