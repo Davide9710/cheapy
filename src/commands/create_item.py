48fc8a9962f2d91261413666f02ab3a51ca8d2ad
Davide <davidelaureti54@gmail.com>
2024-07-16 15:46:42 +0900

    bug fix

--- src/commands/create_item.py
+++ src/commands/create_item.py
@@ -1,6 +1,6 @@
 # Command to initiate the item creation process
 class CreateItemCommand:
-    def execute(self, user, args):
+    def execute(self, user):
         user.state = 'asking_item_name'
         print("Let's create a new item. What's the name of the item?")
         return "Let's create a new item. What's the name of the item?"
