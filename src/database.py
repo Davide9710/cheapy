4dbcf395e8d0ffeb856c786ff981ed506e9baf58
Davide <davidelaureti54@gmail.com>
2024-07-16 15:16:41 +0900

    Fixed renaming bug

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
-from config import SUPABASE_URL, SUPABASE_ANON_KEY
-from models import Item
+from .config import SUPABASE_URL, SUPABASE_ANON_KEY
+from .models import Item
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
